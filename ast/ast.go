b5362862b2de46bc7bf36ec399c4544d43421e0a
cmgn <cmcgov32@gmail.com>
2019-02-11 00:31:07 +0000

    Added AST definitions for type nodes.

--- ast/ast.go
+++ ast/ast.go
@@ -7,6 +7,17 @@ import (
 	"github.com/cmgn/compiler/token"
 )
 
+// UnaryOperatorType is used in the UnaryOperator node to represent
+// the operator type.
+type UnaryOperatorType int
+
+// Unary operator type definitions.
+const (
+	UnaryDereference UnaryOperatorType = iota // '*'
+	UnaryMinus                                // '-'
+	UnaryAddress                              // '&'
+)
+
 // BinaryOperatorType is used in the BinaryOperator node to represent
 // the operator type.
 type BinaryOperatorType int
@@ -38,6 +49,12 @@ type Statement interface {
 type Expression interface {
 	Node
 	expressionNode()
+}
+
+// Type is the interface implemented by all type node types.
+type Type interface {
+	Node
+	typeNode()
 }
 
 // Empty represents an empty statement. The empty statement is used in2
@@ -150,3 +167,76 @@ func (b *BinaryOperator) String() string {
 }
 
 func (b *BinaryOperator) expressionNode() {}
+
+// UnaryOperator represents an occurrence of a unary operator
+// expression.
+type UnaryOperator struct {
+	Type  UnaryOperatorType
+	Value Expression
+}
+
+// SourceInfo gets the source information for the operator inside the
+// unary operator node.
+func (u *UnaryOperator) SourceInfo() *token.SourceInformation {
+	return u.Value.SourceInfo()
+}
+
+func (u *UnaryOperator) String() string {
+	return fmt.Sprintf(
+		"UnaryOperator[%s, %s]",
+		u.Type.String(),
+		u.Value.String(),
+	)
+}
+
+func (u *UnaryOperator) expressionNode() {}
+
+// Primitive is the type for primitive machine types such as integers
+// and characters.
+type Primitive struct {
+	Source token.SourceInformation
+	Type   PrimitiveType
+}
+
+// SourceInfo gets the source information for where the primitive type occurred.
+func (p *Primitive) SourceInfo() *token.SourceInformation {
+	return &p.Source
+}
+
+func (p *Primitive) String() string {
+	return p.Type.String()
+}
+
+func (p *Primitive) typeNode() {}
+
+// PrimitiveType is used in the Primitive node to represent which primitive
+// type is contained in it.
+type PrimitiveType int
+
+// Primitive type definitions.
+const (
+	IntType  PrimitiveType = iota // 'int'
+	CharType                      // 'char'
+)
+
+// ArrayType is the type for fixed-length statically allocated arrays.
+type ArrayType struct {
+	Source token.SourceInformation
+	Size   int
+	Type   Type
+}
+
+// SourceInfo gets the source information for where the array type is defined.
+func (a *ArrayType) SourceInfo() *token.SourceInformation {
+	return &a.Source
+}
+
+func (a *ArrayType) String() string {
+	return fmt.Sprintf(
+		"Array[%s, %d]",
+		a.Type.String(),
+		a.Size,
+	)
+}
+
+func (a *ArrayType) typeNode() {}
